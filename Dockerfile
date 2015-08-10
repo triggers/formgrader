50d5d276689563359b0d7371f265f1246f8b0224
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-08-10 13:03:57 -0700

    Get the formgrader working with proper networking

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM ipython/scipystack
 
 # Install nbgrader
-RUN pip3.4 install nbgrader
+RUN git clone https://github.com/jupyter/nbgrader /srv/nbgrader
+WORKDIR /srv/nbgrader
+RUN pip3.4 install flit
+RUN FLIT_ROOT_INSTALL=1 flit install --env
+RUN flit wheel
+RUN pip3.4 install dist/nbgrader*
 
 # Add nbgrader config
 ADD nbgrader_config.py /etc/ipython/nbgrader_config.py
--- nbgrader_config.py
+++ nbgrader_config.py
@@ -2,14 +2,17 @@ c = get_config()
 
 c.NbGraderConfig.course_id = 'cogsci131'
 
-c.FormgradeApp.ip = "127.0.0.1"
+c.FormgradeApp.ip = "0.0.0.0"
 c.FormgradeApp.port = 9000
 c.FormgradeApp.authenticator_class = "nbgrader.auth.hubauth.HubAuth"
 
 import os
 c.HubAuth.notebook_url_prefix = os.environ['NOTEBOOK_URL_PREFIX']
-c.HubAuth.hubapi_address = os.environ['HUBAPI_ADDRESS']
 c.HubAuth.hub_base_url = os.environ['HUB_BASE_URL']
+c.HubAuth.hubapi_address = "jupyterhub"
+c.HubAuth.proxy_address = "jupyterhub"
+c.HubAuth.hub_address = "jupyterhub"
+c.HubAuth.connect_ip = os.environ['FORMGRADER_IP']
 
 # Add users to the grader list
 graders = set([])
